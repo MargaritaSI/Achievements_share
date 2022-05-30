25e3e0c456eefcd6f6ef47f971250872b16c7773
Margarita Smyslava <margaritasmyslava@Madge-Air.home>
2022-05-31 00:19:06 +0200

    simple_flask_server

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+#from weatherhere import weather_by_city
+
+app = Flask(__name__)
+
+@app.route("/")  # браузер запросил главную страницу
+def index():  # обработчик гл страницы идет на сервер в файл weather и возвр погоду
+    return 'Hello you'
+    # weather = weather_by_city("Amsterdam,Netherlands")
+    # if weather:  # проверка строка существует
+    #     return f"Now {weather['temp_C']}, feels like {weather['FeelsLikeC']}"
+    # else:
+    #     return f"weather service currently is unavailable"
+
+if __name__ == "__main__":
+    app.run(debug=True)
